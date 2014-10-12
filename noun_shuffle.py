15d875184a918ac09272fb26405f480802313653
amandapickering <pickering.amanda@gmail.com>
2014-10-12 17:31:37 -0400

    okay really works now (and cleaned up)

--- noun_shuffle.py
+++ noun_shuffle.py
@@ -14,14 +14,6 @@ def get_nouns(posword_dict):
 	nouns = [str(k) for k, v in posword_dict.iteritems() if (v == u'NNP' or v == u'NN' or v == u'NNS' or v == u'NNPS')]
 	return nouns
 
-# def shuffle_list(some_list): 
-#	just use random.shuffle! whoops
-# 	insert_where = random.randint(0, len(some_list))
-# 	for i in range(0, len(some_list)):
-# 		item = some_list.pop(i)
-# 		some_list.insert(insert_where, item)
-# 	return some_list
-
 def get_indices(token_list, noun_list):
 	indices = []
 	for i in token_list:
@@ -29,17 +21,9 @@ def get_indices(token_list, noun_list):
 			indices.append(token_list.index(i))
 	return indices
 
-# def get_tokens(text):
-# 	with open(text_file) as f:
-# 		text = ' '.join(f.readlines())
-# 		blob = TextBlob(text)
-# 		tokens = list(blob.words)
-# 	return tokens
-
 
 def shuffle_nouns(text_file):
 	#needs some error checking
-	#maybe make getting tokens a different function
 	with open(text_file) as f:
 		text = ' '.join(f.readlines())
 		blob = TextBlob(text)
@@ -50,16 +34,12 @@ def shuffle_nouns(text_file):
 		noun_indices = get_indices(tokens, nouns)
 		random.shuffle(nouns)
 		random.shuffle(extra_nouns)
-
 		for index in noun_indices:
 			if nouns:
 				tokens[index] = nouns.pop()
 			else:
 				tokens[index] = extra_nouns.pop()
-
 		return (' '.join(tokens)).lower()
-
-
 
 if __name__ == '__main__':
 	args = argv[:]
