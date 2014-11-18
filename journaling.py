938b66cab97023ee2b689e7bda0a6e816b6a6ab6
amandapickering <pickering.amanda@gmail.com>
2014-11-18 13:07:59 -0500

    new entries are going to be new files, saving progress before i change everything

--- journaling.py
+++ journaling.py
@@ -3,8 +3,8 @@ import os.path
 import argparse
 from collections import defaultdict
 
-# {datetime: order} i.e. {2014-11-18: 1}
-cache = {}
+# use a database eventually
+ENTRIES = []
 
 def _get_lines():
 	while True:
@@ -14,13 +14,13 @@ def _get_lines():
 		yield line
 
 def create(title):
-	'''creates a journal.txt file in the current directory'''
+	'''creates a journal file in the current directory'''
 	with open(title, 'w') as f:
 		time = str(datetime.datetime.now())
 		f.write(time + '\n')
 		lines = _get_lines()
 		f.write(lines)
-		cache[time] += 1
+		ENTRIES.append(time)
 
 def new_entry(title):
 	'''opens journal.txt for writing, adds a date, time'''
@@ -29,11 +29,12 @@ def new_entry(title):
 		f.write(time + '\n')
 		lines = _get_lines()
 		f.write(lines)
-		cache[time]
+		ENTRIES.append(time)
 
 def read_last_note():
 	'''allows you to read last note 
 	todo: index notes by date? number?'''
+	title = ENTRIES[-1]
 	with open(title, 'r') as f:
 		return f.read()
 
@@ -43,8 +44,8 @@ if __name__ == '__main__':
 	parser.add_argument('-r', '--read', action='store_true')
 	args = parser.parse_args()
 	if args.read:
-		print read_note(args.title)
+		print read_last_note()
 	elif os.path.exists(args.title):
 		new_entry(args.title)
 	else:
-		create(args.title)
+		create('journal.txt')
