7e1687a6246d680eed1b2c1aceb8435b9ff058bd
amandapickering <pickering.amanda@gmail.com>
2014-11-19 13:10:48 -0500

    refactored weatherchecker

--- weatherchecker.py
+++ weatherchecker.py
@@ -1,26 +1,28 @@
 import forecastio
 from pyzipcode import ZipCodeDatabase
 
-api_key = "blah"
-zcdb = ZipCodeDatabase()
+API_KEY = "blah"
 
-isCelsius = raw_input("Celsius or farenheit? Enter C or F: ")
-zipcode = raw_input("Enter your zipcode: ")
+def weatherchecker(zipcode, unit):
+	zcdb = ZipCodeDatabase()
+	if (len(zipcode) > 5 & zipcode.isdigit()):
+		db_zipcode = zcdb[zipcode]
+		lat = db_zipcode.latitude
+		lng = db_zipcode.longitude
+		forecast = forecastio.load_forecast(API_KEY, lat, lng)
+		by_hour = forecast.hourly()
+		current = forecast.currently()
+		if unit == "C":
+			temp = str(((current.temperature)-32)/1.8) + " C"
+		else:	
+			temp = str(current.temperature) + " F"
+		print "The current temperature is " + temp
+		print by_hour.summary
+	else:
+		print "Enter a valid U.S. zipcode."
 
-if (len(zipcode) > 5 & zipcode.isdigit()):
-	db_zipcode = zcdb[zipcode]
-	lat = db_zipcode.latitude
-	lng = db_zipcode.longitude
-	forecast = forecastio.load_forecast(api_key, lat, lng)
+if __name__ == '__main__':
+	is_celsius = raw_input("Celsius or farenheit? Enter C or F: ")
+	zipcode = raw_input("Enter your zipcode: ")
+	weatherchecker(zipcode, is_celsius)
 	
-	byHour = forecast.hourly()
-	current = forecast.currently()
-	if isCelsius == "C":
-		temp = str(((current.temperature)-32)/1.8) + " C"
-	else:	
-		temp = str(current.temperature) + " F"
-	print "The current temperature is " + temp
-	print byHour.summary
-
-else:
-	print "Enter a valid U.S. zipcode."
