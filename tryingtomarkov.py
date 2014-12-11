85fd42579c093eb707162675db90da03bececaaa
amandapickering <pickering.amanda@gmail.com>
2014-12-11 13:21:39 -0500

    dict is working

--- tryingtomarkov.py
+++ tryingtomarkov.py
@@ -1,54 +1,20 @@
 import nltk
-from collections import defaultdict
+from nltk import word_tokenize
+from collections import defaultdict, Counter
 from sys import argv
+from random import choice
 
-"""
-1. tokenize --> list of strings
-2. dictionary
-	string: word that comes next, how often
-3. current string, randomly pick next word from dictionary of words
-"""
-
-#tokenize a text using nltk
-def get_words_from(some_file):
-	tokens = nltk.wordpunct_tokenize(some_file)
-	text = nltk.Text(tokens)
-	words = [w.lower() for w in text]
-	return words
-
-# #dictionary of word and likeliness of next word
-# word_dict = {
-# 	"hello": {"there": 5},
-# 	"there": {"world": 1}
-# }
-# #sample reference
-# word_dict["hello"][1][1]
-
-# list of words in the text
-# tokens = ["hello", "there", "world", "yeah", "cool"]
-
-#loop through list of tokens to make dictionary of likelihoods
-
-# for x in range(len(words)-1):
-# 	if words[x] in word_dict:
-# 		if words[x+1] in word_dict[words[x]]
-# 			word_dict[words[x]][words[x+1]] += 1
-# 		else:
-# 			word_dict[words[x]][words[x+1]] = 1
-# 	else:
-# 		words[x] = {words[x+1]: 1}
-
-def make_markov_dict(some_words):
-	'''where the magic happens (i have no idea what i am doing)
-	some_words is a list of words (the tokenized text)'''
-	zippy_words = zip(some_words, some_words[1:])
-	markov_dict = defaultdict(lambda: defaultdict(int))
-	for a, b in zippy_words:
+def make_markov_dict(text_file, tokens, ngram):
+	with open(text_file) as f:
+		words = word_tokenize(f.read())
+	zippy_words = zip(*[words[i:] for i in xrange(ngram)])
+	markov_dict = defaultdict(Counter)
+	for t in zippy_words:
+		a, b = t[:-1], t[-1]
 		markov_dict[a][b] += 1
 	return markov_dict
 
-def generate_sentence():
-	#makes sentence based on word frequency
+
 
 
 if __name__ == '__main__':
@@ -57,7 +23,4 @@ if __name__ == '__main__':
 	input_file = args[1]
 	with open(input_file) as f:
 		my_text = ' '.join(f.readlines())
-		my_words = get_words_from(my_text)
-		my_markov_dict = make_markov_dict(my_words)
-
-	
+		my_markov_dict = make_markov_dict(my_text)
